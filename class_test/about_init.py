ea5a80dee08ac6af8f5ba283316685ee543de4d9
Ubuntu <ubuntu@ip-172-31-6-7.cn-northwest-1.compute.internal>
2020-09-03 14:10:13 +0800

    update

--- class_test/about_init.py
+++ class_test/about_init.py
@@ -5,14 +5,35 @@ class A():
     def __init__(self):
         self.b = 2
         print(f'this is in init a: {self.a}  b:{self.b}')
+    def test(self):
+        self.d =222
+    
+    @property                                                      
+    def price(self): #查看属性值                                           
+        print ('@property ') 
 
 
 a = A()
 A.a = 10
-A.c = 123
+
 b = A()
 
 b.a = 20
 print(b.a)
 print(a.a)
+A.c = 123
 print(b.c)
+
+b.test()
+print(b.d)
+
+
+class AnimalClassic:
+    pass
+
+#此为新式类，Python 2.2始支持
+class Animal(object):
+    pass
+
+print(dir(AnimalClassic))
+print(dir(Animal))
